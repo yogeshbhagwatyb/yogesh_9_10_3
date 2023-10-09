7e1421409b5a75d8f8a8ea94253bf50cc6b9c6a4
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-10-09 08:50:01 +0530

    Create data.tf

---
+++ tf/data.tf
@@ -0,0 +1,24 @@
+data "aws_availability_zones" "available_1" {
+  state = "available"
+}
+
+data "aws_ami" "amzllinux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+  filter {
+    name = "root-device-type"
+    values = [ "ebs" ]
+  }
+  filter {
+    name = "virtualization-type"
+    values = [ "hvm" ]
+  }
+  filter {
+    name = "architecture"
+    values = [ "x86_64" ]
+  }
+}
